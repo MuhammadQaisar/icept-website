800163928a9c561cbd2da5a96836e01cc98209b3
asadhanif <asadhappy@gmail.com>
2017-09-17 13:15:28 +0500

    Message submission

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -3,11 +3,44 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Message;
 
 class MessagesController extends Controller
 {
-    public function submit()
+    public function submit(Request $request)
     {
-      return 'Data submitted';
-    }
-}
+      $this->validate($request, [
+        'name' => 'required',
+        'email' => 'required'
+      ]);
+
+      $message = new Message;
+      $message->name = $request->input('name');
+      $message->email = $request->input('email');
+      $message->message = $request->input('message');
+
+      // check if user with email exists or not
+
+      // $prev_data = Message::all();
+      //  Message::find(5);
+
+      $prev_data = Message::where('email',
+                              $message->email)
+                              ->get();
+
+      if(count($prev_data) > 0 )
+      {
+        //dont store
+        return redirect('/contact')
+                ->with('errors_msg',
+                'Provided email already exists.');
+      }
+      else
+       {
+         $message->save();
+         return redirect('/')->with('success', 'Message Stored.');
+      }
+    } // end of submit
+
+
+} // end of class
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,10 @@
     <meta charset="utf-8">
     <title> @yield('title') - ICEPT </title>
     <link rel="stylesheet" href="/css/app.css">
+
+    <script type="text/javascript"
+      src="/js/app.js">
+    </script>
   </head>
   <body>
     <header>
@@ -11,6 +15,29 @@
     </header>
 
     <section class="container">
+      @if(count($errors) > 0)
+        @foreach($errors->all() as $error)
+          <div class="alert alert-danger">
+            {{$error}}
+          </div>
+        @endforeach
+      @endif
+
+      @if(session('success'))
+        <div class="alert alert-success alert-dismissable">
+          <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+          <strong>Success!</strong> {{session('success')}}
+        </div>
+      @endif
+
+      @if(session('errors_msg'))
+        <div class="alert alert-danger alert-dismissable">
+          <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+          <strong>Error!</strong> {{session('errors_msg')}}
+        </div>
+      @endif
+
+
       @yield('content')
     </section>
 
